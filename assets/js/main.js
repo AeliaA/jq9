65223a2d4b9eb980a657719112ebb936d8b4585a
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-20 19:20:23 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+
+// $(document).ready(function(){
+// 	$("button").click(function(){
+// 		var txt = "";
+// 		txt += "Width: " + $("#div1").width() + "</br>";
+// 		txt += "Height: " + $("#div1").height();
+// 		$("#div1").html(txt);
+//  	});
+// });
+
+$(document).ready(function(){
+	$("button").click(function(){
+		var txt = "";
+		txt += "InnerWidth: " + $("#div1").innerWidth() + "</br>";
+		txt += "InnerHeight: " + $("#div1").innerHeight();
+		$("#div1").html(txt);
+ 	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
